c38385bb4044de42e38049c52924a84ef03f99d5
SatriaKala11 <142872931+SatriaKala11@users.noreply.github.com>
2023-10-04 00:59:39 +0700

    update dikit

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('menu-toggle input');
-const nav = document.querySelector('navbar-container ul');
-
-menuToggle.addEventListener('click', function () {
-    nav.classList.toggle('slide');
-});
