b49d5c7acad394eba17ff2594c8e2609d77f584e
callistatjonadi <tjonadicallista@gmail.com>
2023-10-04 14:51:57 +0700

    uda bisa sesuai episode juga

--- script.js
+++ script.js
@@ -9,3 +9,7 @@ function redirectToVideo(courseId) {
     window.location.href = `Video_E-Learning.html?courseId=${courseId}&episode=1`;
 }
 
+function tampilinVideo(courseId, episode) {
+    window.location.href = `Video_E-Learning.html?courseId=${courseId}&episode=${episode}`;
+}
+
