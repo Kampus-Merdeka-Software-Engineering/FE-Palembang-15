6b6c3f83ed679fa70e60d1d0476ec64d9d1206e1
SatriaKala11 <142872931+SatriaKala11@users.noreply.github.com>
2023-10-04 17:44:33 +0700

    cek ombak

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const menuToggle = document.querySelector('menu-toggle input');
-const nav = document.querySelector('navbar-container ul');
-
-menuToggle.addEventListener('click', function () {
-    nav.classList.toggle('slide');
-});
 
 function redirectToVideo(courseId) {
     window.location.href = `Video_E-Learning.html?courseId=${courseId}&episode=1`;
